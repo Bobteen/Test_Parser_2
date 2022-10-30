40029a20cb795d62f2079f344719205f1504d5ec
Bobteen <bobteen7@gmail.com>
2022-10-30 16:47:12 +0200

    добавил функцию authorization для входа и авторизации на сайте

--- main.py
+++ main.py
@@ -5,16 +5,27 @@ from time import sleep
 headers = {"User-Agent":
                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"}
 
-work = Session()
 
-work.get('http://quotes.toscrape.com/', headers=headers)  # вход на сайт
+def authorization():
+    work = Session()
 
-responce = work.get('https://quotes.toscrape.com/login', headers=headers)  # вход на страничку с формой авторизации
+    work.get('http://quotes.toscrape.com/', headers=headers)  # вход на сайт
 
-soup = BeautifulSoup(responce.text, 'lxml')
+    responce = work.get('https://quotes.toscrape.com/login', headers=headers)  # вход на страничку с формой авторизации
 
-token = soup.find('form').find('input').get('value') # забираем скрытый токен
+    soup1 = BeautifulSoup(responce.text, 'lxml')
 
-data = {'csrf_token': token, 'username': 'noname', 'password': 'password'} # формируем словарь для заполнения формы
+    token = soup1.find('form').find('input').get('value')  # забираем скрытый токен
 
-result = work.post('https://quotes.toscrape.com/login', headers=headers, data=data, allow_redirects=True)
+    data = {'csrf_token': token, 'username': 'noname', 'password': 'password'}  # формируем словарь для заполнения формы
+    return data
+
+
+# def get_url(work):
+#     result = work.post(f'https://quotes.toscrape.com/login/', headers=headers, data=data,
+#                            allow_redirects=True)
+#     soup2 = BeautifulSoup(result.text, 'lxml')
+#
+#
+# list_card = soup2.find_all('div', class_='quote')
+# text = soup2.find_all('span', class_='text')
